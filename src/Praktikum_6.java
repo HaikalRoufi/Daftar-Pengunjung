de31e089f88d06940aae8f2cb3c1b37d5cf97046
HaikalRoufi <92260742+HaikalRoufi@users.noreply.github.com>
2021-10-31 22:42:32 +0700

    Create Praktikum_6.java

---
+++ src/Praktikum_6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum_6;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Praktikum_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Absen();
+    }
+    
+}
